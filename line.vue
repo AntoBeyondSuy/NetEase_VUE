d7332e922cf93fd69c5c81e5ee8e29796f91ec89
panda052 <1007040724@qq.com>
2019-06-26 18:30:50 +0800

    Add files via upload

---
+++ line.vue
@@ -0,0 +1,141 @@
+<!-- 层叠柱状图 -->
+<style lang="stylus" scoped>
+.line
+  height 1000px
+  background url('../../assets/bg.jpg') no-repeat
+  background-size 100% 100%
+  .main
+    width 100%
+    height calc(100% - 100px)
+    margin-top -15px
+</style>
+
+<template>
+<div class="line">
+  <v-header :name="name" :legendArr="legendArr" :myChart="myChart"></v-header>
+  <v-filter :myChart="myChart" v-if="myChart._dom"></v-filter>
+  <div class="main"></div>
+</div>
+
+</template>
+
+<script>
+import echarts from 'echarts'
+import header from 'components/header/header'
+import filter from 'components/filter/filter'
+
+export default {
+  data() {
+    return {
+      legendArr: [],
+      color: this.$store.state.color,
+      myChart: {},
+      name: '等级分析'
+    }
+  },
+  methods: {
+    _init() {
+      this.legendArr = this.myChart.getOption().series
+      this.legendArr.forEach((data) => {
+        data.selected = true;
+      })
+      this.$root.charts.push(this.myChart)('resize', function() {
+        this.myChart.resize()
+      }.bind(this))
+    }
+  },
+  components: {
+    'v-header': header,
+    'v-filter': filter
+  },
+  mounted() {
+    // 基于准备好的dom，初始化echarts实例
+    this.myChart = echarts.init(document.querySelector('.line .main'));
+    this.myChart.setOption({
+      title: {
+        show: false
+      },
+      tooltip: {
+        trigger: 'axis'
+      },
+      legend: {
+        show: false
+      },
+      toolbox: {
+        show: false
+      },
+      color: this.color,
+      calculable: true,
+      xAxis: [{
+        name: '网易云等级',
+        type: 'category',
+        axisLine: {
+          show: false
+        },
+        axisTick: {
+          show: false
+        },
+        nameTextStyle: {
+          color: 'rgba(255, 255, 255, 0.69)'
+        },
+        axisLabel: {
+          textStyle: {
+            color: 'white'
+          }
+        },
+        data: ['等级零-二', '等级三-四', '等级五-六', '等级七-八', '等级九-十']
+      }],
+      yAxis: [{
+        axisLine: {
+          show: false
+        },
+        nameLocation: 'end',
+        nameGap: 20,
+        nameRotate: 0,
+        axisTick: {
+          show: false
+        },
+        splitLine: {
+          lineStyle: {
+            color: ['rgba(230, 230, 230, 0.2)']
+          }
+        },
+        axisLabel: {
+          textStyle: {
+            color: 'white',
+            fontSize: 14
+          }
+        },
+        name: '用户数',
+        type: 'value',
+        nameTextStyle: {
+          color: 'rgba(255, 255, 255, 0.69)'
+        }
+      }],
+      series: [{
+        name: '男性',
+        type: 'line',
+        data: [3956, 3001, 6779, 7217, 783]
+      }, {
+        name: '女性',
+        type: 'line',
+        data: [2397, 3236, 14865, 30390, 3229]
+      }, {
+        name: '总人数',
+        type: 'line',
+        data: [6353, 6237, 21644, 37607, 5012]
+      }, {
+        name: '平均听歌数',
+        type: 'line',
+        data: [31, 164, 544, 3296, 18622]
+      }, {
+        name: 'VIP',
+        type: 'line',
+        data: [182, 368, 2731, 11867, 2616]
+      }]
+    });
+    this._init()
+  }
+}
+
+</script>
